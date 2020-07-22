f5b18b13434cf345b42d88601077d72cc6fb997a
Roso Sasongko <roso.sasongko@gmail.com>
2020-07-22 10:13:15 +0700

    initial

---
+++ src/components/HelloWorld/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './HelloWorld.module.less';
+
+const HelloWorld = () => {
+    return (
+        <h1 className={style.header}>Hello World</h1>
+    );
+};
+
+export default HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+
+module.exports = {
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true
+                        }
+                    }
+                ],
+                include: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ],
+                exclude: /\.module\.css$/
+            },
+            {
+                test: /\.less$/,
+                exclude: /\.module\.less$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.module\.less$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2,
+                            modules: {
+                                localIdentName: "[path][name]__[local]--[hash:base64:5]"
+                            }
+                        }
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+              ]
+            }
+        ]
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        }
+    }
+};
