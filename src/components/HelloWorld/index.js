a8636a00ea72ce8bed81413c37397ae48e735882
Roso Sasongko <roso.sasongko@gmail.com>
2020-07-22 15:53:40 +0700

    .

--- src/components/HelloWorld/index.js
+++ src/components/HelloWorld/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import style from './HelloWorld.module.less';
 
 const HelloWorld = () => {
-    return (
-        <h1 className={style.header}>Hello World</h1>
-    );
+    return (<h1 className={style.header}>Hello World</h1>);
 };
 
 export default HelloWorld;
